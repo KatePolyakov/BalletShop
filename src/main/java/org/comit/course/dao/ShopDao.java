7544937de93415583125192e858ae880dd9b3287
Kate <72521760+KatePolyakov@users.noreply.github.com>
2022-09-12 07:54:45 -0700

    Add files via upload

---
+++ src/main/java/org/comit/course/dao/ShopDao.java
@@ -0,0 +1,86 @@
+package org.comit.course.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.comit.course.bean.Shop;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ShopDao {
+	
+	public List<Shop> shops;
+	
+	ShopDao() {
+		this.shops = new ArrayList<>();
+		
+		this.shops.add(new Shop(1, "balletshoes1.jpg", "Ballet Shoes", "90.0", "Color: Light Pink | Size: 12, 13", "ModernModern", "A"));
+		this.shops.add(new Shop(2, "balletshoes1.jpg", "Ballet Shoes", "90.0", "Color: Light Pink | Size: 12, 13", "ModernModern", "A"));
+		this.shops.add(new Shop(3, "balletshoes1.jpg", "Ballet Shoes", "90.0", "Color: Light Pink | Size: 12, 13", "ModernModern", "A"));
+		this.shops.add(new Shop(4, "balletshoes1.jpg", "Ballet Shoes", "90.0", "Color: Light Pink | Size: 12, 13", "ModernModern", "A"));
+		this.shops.add(new Shop(5, "balletshoes1.jpg", "Ballet Shoes", "90.0", "Color: Light Pink | Size: 12, 13", "ModernModern", "A"));
+		
+		
+	}
+	
+	public List<Shop> listShops() {
+		 return shops;
+	}
+	
+	public Shop findShop(int idProduct) {
+		
+		Shop shop = new Shop();
+		shop.setIdProduct(idProduct);
+		//this.shops.indexOf(shop);
+		
+		int index = this.shops.indexOf(shop);
+		
+		if (index >=0) {
+			shop = this.shops.get(index);
+		}
+		
+		return shop;
+	}
+	
+	public synchronized void createShop(Shop shop) {
+		
+		int max = 0;
+		for (Shop s: shops) {
+			if (s.getIdProduct()>max) {
+				max= s.getIdProduct();
+			}
+		}
+		
+		shop.setIdProduct(++max);
+		shop.setStatus("A");
+		
+		this.shops.add(shop);
+	}
+	
+/////////////// MODIFY USERS METHOD (FOR SYNCRONISED) /////////////////////
+	
+	public void modifyShop(Shop shop) {
+		
+		//first we need to find user
+		Shop s = this.findShop(shop.getIdProduct());
+		
+		if (s != null) {
+			s.setPhotoName(shop.getPhotoName());
+			s.setName(shop.getName());
+			s.setPrice(shop.getPrice());
+			s.setDescription(shop.getDescription());
+			s.setCompany(shop.getCompany());
+		}
+	}
+	
+	public void deleteShop(int idProduct) {
+		
+		Shop shop = new Shop();
+		shop.setIdProduct(idProduct);
+		//this.shops.indexOf(shop);
+		
+		int index = this.shops.indexOf(shop);
+		this.shops.remove(index);
+	}
+
+}
---
+++ src/main/java/org/comit/course/service/ShopService.java
@@ -0,0 +1,56 @@
+package org.comit.course.service;
+
+import java.util.List;
+
+import org.comit.course.bean.Shop;
+import org.comit.course.dao.ShopDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShopService {
+	
+	@Autowired
+	ShopDao shopDao;
+///////////// LIST USERS METHOD ///////////////////////
+	
+	public List<Shop> listShops(){
+		
+		List<Shop> shops = shopDao.listShops();
+		
+		shops.removeIf(shop-> shop.getStatus() != "A");
+		
+		return shops;
+	}
+	/////////////// FIND USERS METHOD /////////////////////
+	public Shop findShop(int idProduct) {
+		
+		return shopDao.findShop(idProduct);
+	}
+	
+	/////////////// CREATE USERS METHOD /////////////////////
+	public void createShop(Shop shop) {
+		
+		this.validateShop(shop);
+		shopDao.createShop(shop);
+	}
+	
+/////////////// MODIFY USERS METHOD (FOR SYNCRONISED) /////////////////////
+	public void modifyShop(Shop shop) {
+		this.validateShop(shop);
+		shopDao.modifyShop(shop);
+	}
+	
+/////////////// DELETE USERS METHOD /////////////////////
+	public void deleteShop(int idProduct) {
+		
+		shopDao.deleteShop(idProduct);
+	}
+	////////VALIDATION OF USER /////////
+	private void validateShop(Shop shop) {
+		if(shop.getName().isEmpty() || shop.getPrice().isEmpty() || shop.getDescription().isEmpty() || shop.getCompany().isEmpty()) {
+			throw new RuntimeException("Invalid Data" + shop);
+		}
+	}
+
+}
